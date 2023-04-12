f37caee70792d7cd5492a006ca047461e6de60d7
lim6112j <lim6112j@gmail.com>
2023-04-12 23:15:04 +0900

    index

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import _ from 'lodash';
 import csvtojson from 'csvtojson';
-
 // load csv file
 const csvfilepath = 'latlng_korea.csv';
 const jsonWithCsv = async (filepath: string) => {
@@ -9,9 +8,15 @@ const jsonWithCsv = async (filepath: string) => {
 }
 // find string in a row
 jsonWithCsv(csvfilepath).then((res) => {
-  const query = `읍면동/구`;
-  const query2 = `읍/면/리/동`;
-  console.log(query);
-  const found = _.find(res, function(o) { return o[query2] == "모리" });
-  console.log(found);
+  const query1 = `시도`;
+  const query2 = `시군구`;
+  const query3 = `읍면동/구`;
+  const query4 = `읍/면/리/동`;
+  const query5 = `리`;
+  const val = process.argv[2]
+  console.log(val);
+  const found = _.find(res, function(o) { 
+    return o[query1] == val || o[query2] == val || o[query3] == val ||  o[query4] == val ||  o[query5] == val 
+  });
+  console.log(val, found['위도'], found['경도']);
 });
